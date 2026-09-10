49d63167a73a07a121f82e4e17f3235cd5a074b5
noumanovnaanastasia-netizen <noumanovnaanastasia@gmail.com>
2026-09-10 19:18:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 import os
 
-# 🔒 Переменные окружения (подключаются в настройках Render)
+# 🔒 Переменные окружения (настраиваются в панели управления Render)
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
-# 🎨 Ссылки на твои потрясающие баннеры с Telegra.ph
+# 🎨 Ссылки на твои потрясающие баннеры из канала @banerss777
 PHOTO_MAIN_MENU = "https://t.me/banerss777/9"
 PHOTO_TARIFFS = "https://t.me/banerss777/8"
 PHOTO_INSTRUCTIONS = "https://t.me/banerss777/7"
 PHOTO_PROMO = "https://t.me/banerss777/6"
 PHOTO_SUPPORT = "https://t.me/banerss777/11"
 
+# 📖 Официальные ссылки на твои статьи в Telegra.ph
+URL_AGREEMENT = "https://telegra.ph/Polzovatelskoe-soglashenie-i-Politika-konfidencialnosti-Kiffis-Tunnel-09-10"
+URL_INSTR_IOS = "https://telegra.ph/Podrobnaya-instrukciya-dlya-iOS-iPhone--iPad-09-10"
+URL_INSTR_ANDROID = "https://telegra.ph/Podrobnaya-instrukciya-dlya-Android-09-10"
 
 # 🕊 Текстовые ссылки на бесплатные пулы из проверенных репозиториев
 URL_AVENCORES_VPN = "https://github.com/AvenCores/goida-vpn-configs"
@@ -20,6 +24,7 @@ URL_AVENCORES_COMBO = "https://githubusercontent.com"
 URL_IGARECK_WHITELIST = "https://github.com/igareck/vpn-configs-for-russia"
 
 # 📊 Утвержденная тарифная сетка в Telegram Stars (⭐)
+# Сроки: 3_days, 1_month, 3_months. (Срок 14_days будет считаться кодом автоматически)
 TARIFFS = {
     "base": {  # Настоящие Прокси и обычный VPN
         "3_days": {1: 7, 2: 10, 3: 12, 4: 13, 5: 14},
@@ -33,13 +38,13 @@ TARIFFS = {
     }
 }
 
-# 🎟 Стартовый набор твоих авторских промокодов со скидками и бонусами
+# 🎟 Стартовый набор твоих авторских промокодов для первой заливки в базу данных Supabase
 PROMOCODES_STARTUP = {
     "KiffissTunnel202": {"type": "percent", "value": 20, "uses": 2},
     "Gissitor": {"type": "stars", "value": 5, "uses": 2},
     "Hoprtt": {"type": "stars", "value": 10, "uses": 2},
-    "BeSpLaTnO": {"type": "bonus_days", "value": 2, "uses": 3},   # 2 дня бесплатного комбо
-    "LbgoTa": {"type": "bonus_days", "value": 3, "uses": 1},       # 3 дня белых списков
-    "SakeraHarena": {"type": "bonus_days", "value": 5, "uses": 3}, # 5 дней белых списков
+    "BeSpLaTnO": {"type": "bonus_days", "value": 2, "uses": 3},      # 2 дня бесплатного комбо
+    "LbgoTa": {"type": "bonus_days", "value": 3, "uses": 1},         # 3 дня белых списков
+    "SakeraHarena": {"type": "bonus_days", "value": 5, "uses": 3},    # 5 дней белых списков
     "QhcpTTlo": {"type": "percent_condition", "value": 40, "uses": 5} # -40% если цена от 50 звезд
 }
