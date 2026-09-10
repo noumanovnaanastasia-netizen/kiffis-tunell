eb6cde85ffcbeebe2238b8f5fd8ea2617c586f68
noumanovnaanastasia-netizen <noumanovnaanastasia@gmail.com>
2026-09-10 18:44:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiohttp import web
 
 # 1. Твой токен от @KIFFISST_BOT
-BOT_TOKEN = "8945413131:AAEoscGljaqoMZoz95CFyiy7OboLT5cheys"
+BOT_TOKEN = "8945413131:AAGz5xrT9_e9uLd1i2XE8gfzkUWjji469Qc"
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
