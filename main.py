1f6c2e2bd3a4ba4361b4648642947487697a2283
noumanovnaanastasia-netizen <noumanovnaanastasia@gmail.com>
2026-09-10 14:15:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 # 2. Чистый запуск бота
-bot = Bot(token="8945413131:AAEoscGljaqoMZoz95CFyiy7OboLT5cheys"
+bot = Bot(token="8945413131:AAEoscGljaqoMZoz95CFyiy7OboLT5cheys")
 dp = Dispatcher()
 
 # Обработчик команды /start
